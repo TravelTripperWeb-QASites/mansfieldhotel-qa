d5d2ca7d50ca959cdb2876c2bee3869377d87c29
dmanikym <dmanikyam@traveltripper.com>
2017-09-06 15:48:19 +0530

    Nav scroll resize

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -11,10 +11,19 @@ $(document).ready(function () {
 	$("a").smoothScroll({
 		offset: -20
 	});
-	
+
 	//homeBannercarousel
 	$('.carousel').carousel();
 
 	$('#carouselHomebanner .carousel-item').css('max-height', window.innerHeight - 90);
+});
 
+$(window).scroll(function() {    
+	var scroll = $(window).scrollTop();
+	if (scroll >= 60) {
+		//clearHeader, not clearheader - caps H
+		$(".navbar").addClass("resized");
+	}else {
+        $(".navbar").removeClass("resized");
+    }
 });
